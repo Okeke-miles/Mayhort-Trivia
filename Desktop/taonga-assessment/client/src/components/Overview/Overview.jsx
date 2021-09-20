04f4265045c35edb29671101664396830a9c623c
Okeke-miles <franklinokeke9@gmail.com>
2021-09-20 18:11:17 -0400

    unresolved section completed

--- Desktop/taonga-assessment/client/src/components/Overview/Overview.jsx
+++ Desktop/taonga-assessment/client/src/components/Overview/Overview.jsx
@@ -28,6 +28,41 @@ function Overview() {
                         <p className="quantity__style">64</p>
                     </div>
                 </div>
+                <article className="unresolved-section__style">
+                    <div className="unresolved-container__style">
+                        <div className="inner-flex__style">
+                            <main className="unresolved__style">Unresolved tickets <p className="support__style">Group: Support</p></main>
+                            <p className="unresolved-desc__style">Waiting on Feature Request</p>
+                            <p className="unresolved-desc__style">Awaiting Customer Response</p>
+                            <p className="unresolved-desc__style">Awaiting Developer Fix</p>
+                            <p className="unresolved-desc__style">Pending</p>
+                        </div>
+                        <div className="outer-flex__style">
+                            <p className="quantity__style">View details</p>
+                            <p className="unresolved-qty__style">4238</p>
+                            <p className="unresolved-qty__style">1005</p>
+                            <p className="unresolved-qty__style">914</p>
+                            <p className="unresolved-qty__style">281</p>
+                        </div>
+                    </div>
+                    <div className="unresolved-task-container__style">
+                        <div className="inner-task-flex__style">
+                            <main className="unresolved__style">Tasks <p className="support__style">Today</p></main>
+                            <p className="unresolved-task__style">Create new task</p>
+                            <p className="unresolved-task__style">Finish ticket update</p>
+                            <p className="unresolved-task__style">Create new ticket example</p>
+                            <p className="unresolved-task__style">Update ticket report</p>
+                        </div>
+                        <div className="outer-flex__style">
+                            <p className="quantity__style">View all</p>
+                            <p className="unresolved-task-qty__style plus__style">+</p>
+                            <p className="unresolved-task-qty__style urgent__style">URGENT</p>
+                            <p className="unresolved-task-qty__style new__style">NEW</p>
+                            <p className="unresolved-task-qty__style default__style">DEFAULT</p>
+                        </div>
+                    </div>
+
+                </article>
             </div>
         </div>
     )
